e1455a355a7292f2f05805d32681df2f70f25da2
Raphael Basso <arabasso@host.sk>
2023-12-06 10:00:49 -0300

    Initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createTheme, ThemeMode, ThemeProvider } from "@rneui/themed";
+import { useColorScheme } from "react-native";
+import Home from "./screens/Home";
+
+const theme = createTheme({
+  lightColors: {},
+  darkColors: {},
+});
 
 export default function App() {
+  theme.mode = useColorScheme() as ThemeMode;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
